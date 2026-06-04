355a13a5ac993d51ac2f8e154d33cb2fde902458
Aryamods <96381993+aryamods@users.noreply.github.com>
2026-06-05 01:21:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1447,4 +1447,5 @@ async def generate_pdf(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
